41d2200fb28d70fda5f44a2afd9d1ec76340198b
Shawn Canavan <shawncanavan@Shawns-MacBook-Pro-2.local>
2022-11-20 20:22:28 -0500

    added some todos on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+  whiteB,
+  grayB,
+  lightBB,
+  darkBB,
+  whiteC,
+  grayC,
+  lightBC,
+  darkBC,
+} from './ProjectComponents/colors';
 
 const classes = {
   footer: 'border border-primary mt-5',
 };
-
+//*contact
+//*Navigation to other sections
+//*facebook
+//*linkedIn
+//*copyright
 function Footer() {
   return (
     <footer className={classes.footer}>
